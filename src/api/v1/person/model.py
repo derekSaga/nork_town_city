2ecddf1381a1e4a8b27bd1d46f7c6bca4c542a8a
Derek da Silva <derek.coelho@outlook.com.br>
2023-03-05 20:57:53 -0300

    feat: created person model

--- src/api/v1/person/model.py
+++ src/api/v1/person/model.py
@@ -1,8 +1,11 @@
+from sqlalchemy import Column
 from sqlalchemy import types
+from sqlalchemy.orm import relationship
 
-from models.base_model import BaseModelMixin
-from models.base_model import db
+from api.models.base_model import BaseModelMixin
+from api.v1.car.model import Car
 
 
-class PersonModl(BaseModelMixin):
-    name = db.Column(types.String(155), nullable=False)
+class Person(BaseModelMixin):
+    name = Column(types.String(155), nullable=False)
+    cars = relationship(Car, backref="car", passive_deletes=True)
