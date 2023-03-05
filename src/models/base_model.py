6ff86dca3911ba076bbb145e1f95732726b191ce
Derek da silva <derek.coelho@outlook.com.br>
2023-03-05 12:57:49 -0300

    feat: create model base

---
+++ src/models/base_model.py
@@ -0,0 +1,12 @@
+import uuid
+
+from sqlalchemy import types
+
+from main import db
+
+
+class BaseModelMixin(db.Model):
+    __abstract__ = True
+    id = db.Collumn(types.UUID, default=uuid.uuid4())
+    created_on = db.Column(db.DateTime, default=db.func.now())
+    updated_on = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
