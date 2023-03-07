77b892a571919da4f91f84c2ac88c889bdcca7dd
Derek da Silva <derek.coelho@outlook.com.br>
2023-03-06 23:12:58 -0300

    refacot: included new variable db

--- src/api/models/base_model.py
+++ src/api/models/base_model.py
@@ -1,12 +1,14 @@
 import uuid
 
-from sqlalchemy import types
+from flask_sqlalchemy import SQLAlchemy
 
-from api import db
+db = SQLAlchemy()
 
 
 class BaseModelMixin(db.Model):
     __abstract__ = True
-    id = db.Column(types.UUID, default=uuid.uuid4(), primary_key=True)
+    id = db.Column(db.UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
     created_on = db.Column(db.DateTime, default=db.func.now())
-    updated_on = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    updated_on = db.Column(
+        db.DateTime, default=db.func.now(), onupdate=db.func.now()
+    )
