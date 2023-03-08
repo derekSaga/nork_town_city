0deb23dfd6ad5b16f09acd87cb98cce52f3a888b
Derek da silva <derek.coelho@outlook.com.br>
2023-03-08 13:23:37 -0300

    feat: created route GET car

--- src/api/v1/car/views/car_view.py
+++ src/api/v1/car/views/car_view.py
@@ -1,4 +1,5 @@
-from apiflask import APIBlueprint
+from uuid import UUID
+from apiflask import APIBlueprint, HTTPError
 
 from api.v1.car.schemas import CarSchema
 from api.v1.car.views.busisness import car_busisness
@@ -16,6 +17,15 @@ def create_car(car_data: CarSchema) -> CarSchema:
 
 @car.delete("/<uuid:car_id>")
 @car.output({}, status_code=204)
-def delete_pet(car_id):
+def delete_car(car_id):
     car_busisness.delete_car(car_id)
     return ""
+
+
+@car.get("/<uuid:car_id>")
+@car.output(CarSchema)
+def get_car(car_id: UUID):
+    result = car_busisness.get_car(car_id)
+    if result is None:
+        raise HTTPError(404, "Car ID %s: Not Found" % car_id, detail=None)
+    return result
