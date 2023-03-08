dd1a4dd95535a9dc2fb3e58c3d73e0a9c3ecde21
Derek da silva <derek.coelho@outlook.com.br>
2023-03-08 13:56:17 -0300

    feat: created person busisness module

---
+++ src/api/v1/person/views/busisness/person_busisness.py
@@ -0,0 +1,32 @@
+from uuid import UUID
+from api.v1.person.model import Person
+from api.models.base_model import db
+from api.v1.person.schema import PersonSchema
+
+
+def retrieve_person(user_id: UUID) -> Person:
+    return Person.query.get_or_404(user_id)
+
+
+def delete_person(user_id: UUID) -> None:
+    person = retrieve_person(user_id)
+    session = db.session
+    try:
+        db.session.delete(person)
+        db.session.commit()
+        return
+    except Exception as e:
+        session.rollback()
+        raise e
+
+
+def create_person(person_data: PersonSchema) -> PersonSchema:
+    new_person = Person(**person_data)
+    session = db.session
+    try:
+        session.add(new_person)
+        session.commit()
+        return new_person
+    except Exception as e:
+        session.rollback()
+        raise e
